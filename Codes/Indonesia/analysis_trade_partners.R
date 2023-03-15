9fa8c907c2ddd878ed2bd44df1ec3bfe4664d08c
simncg <simncg@gmail.com>
2023-03-15 10:01:19 -0400

    Creates function to compute the tables of trade partners analysis

--- Codes/Indonesia/analysis_trade_partners.R
+++ Codes/Indonesia/analysis_trade_partners.R
@@ -75,9 +75,7 @@ aberdeen_f_data<-fread("../../Data/Indonesia/raw_data/matched_foreign_firms_indo
   # SIC industry, NAICS6 classficiation. 
   select(foreign_company_id = our_ID, foreign_country_aberdeen = country_iso3_01, foreign_SITEID = SITEID,
          foreign_aberdeen_firm_name = Aberdeen_COMPANY,  foreign_panjiva_firm_name = panjiva_cleaned_name_01,
-         foreign_SICGRP = SICGRP, foreign_NAICS6_CODE = NAICS6_CODE)%>% 
-  lazy_dt() %>% 
-  as_tibble()
+         foreign_SICGRP = SICGRP, foreign_NAICS6_CODE = NAICS6_CODE)
 
 ### Aberdeen Domestic ----
 
@@ -113,14 +111,14 @@ aberdeen_data<-left_join(aberdeen_data, aberdeen_data_2, by = "company_id") %>%
          domestic_company_id = company_id, 
          aberdeen_firm_name =Aberdeen_COMPANY, 
          panjiva_firm_name = panjiva_domestic_1) %>% 
-  lazy_dt() %>% 
-  as_tibble()
+  lazy_dt() 
 
 rm(aberdeen_data_2, IDN_Domestic_Ids_Corresp)
 
 
 ## Panjiva Exports for foreign firms ----
 
+# This data is at the domestic firm-foreign firm-year-month-hs8 level
 exports_f_data<-read_dta("../../Data/Indonesia/raw_data/IDN_Exports_Monthly_foreign_domestic_level_dropdup.dta") %>% 
   lazy_dt() %>% 
   rename(foreign_country_panjiva = country) %>% 
@@ -135,11 +133,11 @@ exports_f_data<-read_dta("../../Data/Indonesia/raw_data/IDN_Exports_Monthly_fore
   # Join Aberdeen data for foreign firms
   left_join(aberdeen_f_data, by = "foreign_company_id") %>%
   # Join Aberdeen data for domestic firms
-  left_join(aberdeen_data, by = "domestic_company_id") %>%
-  as_tibble()
+  left_join(aberdeen_data, by = "domestic_company_id") 
 
 ## Panjiva Imports for foreign firms ----
 
+# This data is at the domestic firm-foreign firm-year-month-hs8 level
 imports_f_data<-read_dta("../../Data/Indonesia/raw_data/IDN_Imports_Monthly_foreign_domestic_level_dropdup.dta") %>% 
   lazy_dt() %>% 
   rename(foreign_country_panjiva = country) %>% 
@@ -154,55 +152,187 @@ imports_f_data<-read_dta("../../Data/Indonesia/raw_data/IDN_Imports_Monthly_fore
   # Matching rates
   mutate(matched_aberdeen_dom = domestic_company_id %in% aberdeen_data$domestic_company_id, 
          matched_aberdeen_for = foreign_company_id %in% aberdeen_f_data$foreign_company_id, 
-         matched_both_aberdeen = as.logical(matched_aberdeen_dom*matched_aberdeen_for)) %>% 
+         matched_both_aberdeen = as.logical(matched_aberdeen_dom*matched_aberdeen_for))
+
+
+## HS products information ----
+
+# Read HS Code Classification data with products information ----
+hs_data <- read_csv("../../Data/Extra Data/HS_code_classifications.csv")%>% 
+  mutate(hs_2017   = str_pad(hs_2017  , 6, "left", "0")) %>% 
+  lazy_dt()
+
+# Read data with intermediate and capital HS products classification ----
+int_cap_HS <- read_excel("../../Data/Extra Data/capital_intermediate_HS_code_classification.xlsx", 
+                         sheet = "FromHSToISICToEC") %>% 
+  # Keep only HS 2012 version
+  filter(HS == 4) %>% 
+  # Keep only the variable with the HS6 codes and the variable EUC with information of the type of product
+  select(hs6 =`HS-6digit`, EUC) %>% 
+  # Join with correspondence tables to convert from HS codes 2012 to HS codes 2017 
+  left_join(., read_xlsx("../../Data/Extra Data/HS2017toHS2012ConversionAndCorrelationTables.xlsx") %>% 
+              rename( "hs_2017" = "From HS 2017",  "hs_2012"="To HS 2012"), 
+            by = c("hs6" = "hs_2012")) %>% 
+  select(hs_2017, EUC) %>% 
+  # Create an indicator variable 
+  mutate(value = 1) %>% 
+  # Remove na in hs codes 2017
+  na.omit(hs_2017) %>% 
+  # Long format to wide format: convert CAP and INT to two separate variables
+  pivot_wider(names_from = EUC, values_from = value, values_fill = 0) %>% 
+  # Keep only the identifiers variables for intermediate and capital products
+  select(hs_2017, CAP, INT) %>% 
+  # From dummy to logical 
+  mutate(CAP = as.logical(CAP), 
+         INT = as.logical(INT)) %>% 
+  lazy_dt()
+
+gc()
+
+# Tables ----
+
+# Data of imports that we can analyze, so those observations that have both 
+# non-missing values in SIC Group of aberdeen domestic and aberdeen foreign
+
+included_SIC_Groups <- c('AG-M-C', # Agricultural, Construction and Mining
+                         "MANUF",  # Manufacturing
+                         "SVCS",   # Services
+                         "F-I-RE", # Finance Insurance, Real Estate
+                         "TR-UTL", # Transport & Utilities
+                         "WHL-RT"  # Wholesale-retail
+)
+
+## Table 1 ----
+
+# Create table with summary statistics between local industry and foreign 
+# industry. We will analyze three categories of industries Wholesale-Retail, Manufacturing
+# and the rest (Agricultural, Construction and Mining, Services, Finance Insurance and Real State,
+# Transport & Utilities). 
+
+imports_analysis<-imports_f_data %>% 
+  na.omit(SICGRP, foreign_SICGRP) %>% 
+  filter(SICGRP %in% included_SIC_Groups, 
+         foreign_SICGRP %in% included_SIC_Groups) %>% 
+  # Categorize in 3 industries (Wholesale-retail, Manufacturing and the rest)
+  mutate(industry_local = if_else(!(SICGRP %in% c("MANUF", "WHL-RT")), "Rest", SICGRP), 
+         industry_foreign = if_else(!(foreign_SICGRP %in% c("MANUF", "WHL-RT")), "Rest", foreign_SICGRP)
+        ) %>% 
+  # Filter for USA-IDN trade flow relationship
+  filter(foreign_country_panjiva == "USA") %>% 
+  # Group by local industry and foreign industry combinations (so 9 combinations)
+  group_by(industry_local, industry_foreign) %>%
+  # Create some summary statistics 
+  mutate(
+    # Total number of domestic companies in industry X that trade with foreign firms in industry X
+    n_domestic_firms = n_distinct(domestic_company_id), 
+    # Compute the total value of imports from domestic firms (sum of imports across firms) in industry X to all foreign firms in industry X.
+    tot_imp_to_f_ind = sum(import), 
+    # Average per IDN firm of total value of imports from domestic firms in industry X to all foreign firms in industry X
+    avg_tot_imp_to_f_ind = tot_imp_to_f_ind/n_domestic_firms 
+  ) %>% 
+  ungroup() %>% 
+  # Group by domestic firms, local industry and foreign industry
+  group_by(domestic_company_id, industry_local, industry_foreign) %>% 
+  # Number of foreign exporters by domestic firm (exporters because domestic companies are importing from them)
+  mutate(foreign_n_exporters = n_distinct(foreign_company_id)) %>% 
+  ungroup() %>% 
+  # Group by local industry and foreign industry 
+  group_by(industry_local, industry_foreign) %>% 
+  mutate(
+    # Average number of foreign partners per firm by local industry and foreign industry 
+    avg_n_exporters = mean(foreign_n_exporters),  
+    ) %>% 
+  ungroup() %>% 
+  # Group by domestic firm, foreign firm, local industry and foreign industry
+  group_by(domestic_company_id, foreign_company_id, industry_local, industry_foreign) %>% 
+  # Sum the total value of imports between two firms (firm-to-firm imports)
+  mutate(imports_firm_to_firm = sum(import)) %>% 
+  ungroup() %>% 
+  # Group by local industry and foreign industry
+  group_by(industry_local, industry_foreign) %>% 
+  # Take the average of the firm-to-firm imports 
+  mutate(mean_imp_firm_to_firm = mean(imports_firm_to_firm)) %>% 
+  ungroup() %>% 
+  select(industry_local, industry_foreign, n_domestic_firms, avg_tot_imp_to_f_ind, 
+         avg_n_exporters, mean_imp_firm_to_firm) %>% 
+  distinct(industry_local, industry_foreign, .keep_all = T) %>% 
   as_tibble()
 
 
 
+## Table 2: Same as table 1 but using products classification ----
+
+# NOTE: There is a double counting in the number of firms, but this is just to see patterns
+
+imp_prod_analysis<-imports_f_data %>% 
+  left_join(hs_data, by = c("hs6" = "hs_2017")) %>% 
+  left_join(int_cap_HS, by = c("hs6" = "hs_2017")) %>% 
+  na.omit(SICGRP, foreign_SICGRP) %>% 
+  filter(SICGRP %in% included_SIC_Groups, 
+         foreign_SICGRP %in% included_SIC_Groups) %>% 
+  # Categorize in 3 industries (Wholesale-retail, Manufacturing and the rest)
+  mutate(industry_local = if_else(!(SICGRP %in% c("MANUF", "WHL-RT")), "Rest", SICGRP), 
+         industry_foreign = if_else(!(foreign_SICGRP %in% c("MANUF", "WHL-RT")), "Rest", foreign_SICGRP)
+  )%>% 
+  # Filter for USA-IDN trade flow relationship
+  filter(foreign_country_panjiva == "USA") 
 
 
 
-# 
-# match_imports<- data.frame(Data = "Panjiva Imports",
-#                            n_dom_firms = length(unique(imports_f_data$domestic_company_id)), 
-#                            n_for_firms = length(unique(imports_f_data$foreign_company_id))
-#                 ) %>% 
-#   mutate(n_firms_total = n_dom_firms + n_for_firms) %>% 
-#   bind_cols(
-#     imports_f_data %>% 
-#       select(domestic_company_id,matched_aberdeen_dom) %>% 
-#       distinct(domestic_company_id, .keep_all = T) %>% 
-#       mutate(n_match_dom = sum(matched_aberdeen_dom)) %>% 
-#       select(n_match_dom) %>% 
-#       head(1)
-#   ) %>% 
-#   bind_cols(
-#     imports_f_data %>% 
-#       select(foreign_company_id, matched_aberdeen_for) %>% 
-#       distinct(foreign_company_id, .keep_all = T) %>% 
-#       mutate(n_match_for = sum(matched_aberdeen_for)) %>% 
-#       select(n_match_for) %>% 
-#       head(1)
-#   ) %>% 
-#   mutate(`% Matched to Aberdeen Domestic` = n_match_dom/n_dom_firms,
-#          `% Matched to Aberdeen Foreign` = n_match_for/n_for_firms
-#   )
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# # SIC Groups to analyze ----
-# included_SIC_Groups <- c('AG-M-C', # Agricultural, Construction and Mining
-#                          "MANUF",  # Manufacturing
-#                          "SVCS",   # Services
-#                          "F-I-RE", # Finance Insurance, Real Estate 
-#                          "TR-UTL", # Transport & Utilities
-#                          "WHL-RT"  # Wholesale-retail
-# )
-# 
-# 
+# This function generates same descriptive statistics (number of domestic firms in local industry trading
+# with foreign industries, average value of exports per domestic firm, etc), but we take into account 
+# another dimension, which is the product classification. 
+
+foreign_prod<-function(data, product_var, short_name, non_short_name){
+  table_products<-
+    data%>% 
+    group_by({{product_var}}, industry_local, industry_foreign) %>% 
+    mutate(
+      # Total number of domestic companies in industry X that trade with foreign firms in industry X
+      n_domestic_firms = n_distinct(domestic_company_id), 
+      # Compute the total value of imports from domestic firms (sum of imports across firms) in industry X to all foreign firms in industry X.
+      tot_imp_to_f_ind = sum(import), 
+      # Average per IDN firm of total value of imports from domestic firms in industry X to all foreign firms in industry X
+      avg_tot_imp_to_f_ind = tot_imp_to_f_ind/n_domestic_firms
+    ) %>% 
+    ungroup() %>% 
+    # Group by domestic firms, local industry and foreign industry
+    group_by(domestic_company_id, {{product_var}}, industry_local, industry_foreign) %>% 
+    # Number of foreign exporters by domestic firm (exporters because domestic companies are importing from them)
+    mutate(foreign_n_exporters = n_distinct(foreign_company_id)) %>% 
+    ungroup() %>% 
+    # Group by local industry and foreign industry 
+    group_by({{product_var}}, industry_local, industry_foreign) %>% 
+    mutate(
+      # Average number of foreign partners per firm by local industry and foreign industry 
+      avg_n_exporters = mean(foreign_n_exporters),  
+    ) %>% 
+    ungroup() %>% 
+    # Group by domestic firm, foreign firm, local industry and foreign industry
+    group_by({{product_var}}, domestic_company_id, foreign_company_id, industry_local, industry_foreign) %>% 
+    # Sum the total value of imports between two firms (firm-to-firm imports)
+    mutate(imports_firm_to_firm = sum(import)) %>% 
+    ungroup() %>% 
+    # Group by local industry and foreign industry
+    group_by({{product_var}}, industry_local, industry_foreign) %>% 
+    # Take the average of the firm-to-firm imports 
+    mutate(mean_imp_firm_to_firm = mean(imports_firm_to_firm)) %>% 
+    ungroup() %>% 
+    select({{product_var}}, industry_local, industry_foreign, 
+           n_domestic_firms, avg_tot_imp_to_f_ind, avg_n_exporters, mean_imp_firm_to_firm) %>% 
+    distinct({{product_var}}, industry_local, industry_foreign, .keep_all = T) %>% 
+    mutate({{product_var}} = if_else({{product_var}} == T, short_name, non_short_name)) %>% 
+    # I will do the pivot wider
+    pivot_wider(
+      names_from = {{product_var}}, 
+      values_from = c(n_domestic_firms:mean_imp_firm_to_firm)
+    ) %>% 
+    as_tibble()
+  
+  return(table_products)
+  
+}
+
+
+
+kaka<-foreign_prod(imp_prod_analysis, China_E_commerce, "E_com", "Non_ecom")
