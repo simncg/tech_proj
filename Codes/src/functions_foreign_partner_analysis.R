b7066f5fc250227f376bf1e7270612fbe421a0d6
simncg <simncg@gmail.com>
2023-04-07 11:09:49 -0400

    Functions for creating foreign partner analysis

--- Codes/src/functions_foreign_partner_analysis.R
+++ Codes/src/functions_foreign_partner_analysis.R
@@ -112,3 +112,234 @@ foreign_prod_exp<-function(data, product_tech_var){
   
 }
 
+
+
+# Functions to create plots ----
+
+
+plot_total_values<-function(exports_data, imports_data){
+  
+  
+  p1<-exports_data %>% 
+    mutate(
+      industry_local = case_when(
+        industry_local == "MANUF" ~ "Manufacturing",
+        industry_local == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_local
+      ),
+      industry_foreign = case_when(
+        industry_foreign == "MANUF" ~ "Manufacturing",
+        industry_foreign == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_foreign
+      )
+    ) %>% 
+    mutate(avg_tot_exp_to_f_ind = avg_tot_exp_to_f_ind/1000000, mean_exp_firm_to_firm = mean_exp_firm_to_firm/1000000) %>% 
+    rename(`No. Indonesian Firms` = n_domestic_firms, `No. US Firms` = n_foreign_firms, `Avg. Export Value per IDN \n Firm to US Industry (USD Million)` = avg_tot_exp_to_f_ind, 
+           `Avg. No US Importers \n per IDN firm` = avg_n_importers, `Avg. Value of Firm-to-Firm Exports \n (USD Million)` = mean_exp_firm_to_firm
+    ) %>% 
+    pivot_longer(cols = -c(industry_local, industry_foreign), 
+                 names_to = "variable") %>% 
+    mutate(industries = paste("IDN", industry_local, "-", "US", industry_foreign)) %>% 
+    ggplot(aes(x = variable, y = value, fill = industries))+
+    geom_col(position = position_dodge(width = 1.1), width = 1)+
+    labs(fill = "Bilateral Industry Relation:")+
+    xlab("")+
+    ylab("")+
+    ggtitle("")+
+    theme_minimal()+
+    theme(legend.key.size = unit(0.3, 'cm'), 
+          legend.text = element_text(size = 5), 
+          legend.title = element_text(size = 9, face = "bold"), 
+          strip.text = element_blank(), 
+          axis.text.x = element_text(size = 6), 
+          axis.text.y = element_text(size = 6), 
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(), 
+          plot.title = element_text(size = 12, face = "bold", hjust = 0.5))+
+    guides(colour = guide_legend(override.aes = list(size = 3)),
+           shape = guide_legend(override.aes = list(size = 3), keyheight = 0.6))+
+    scale_fill_economist()+
+    facet_wrap(~variable, scales = "free", nrow = 3)
+  
+  
+  
+  
+  p2<-imports_data %>% 
+    mutate(avg_tot_imp_to_f_ind = avg_tot_imp_to_f_ind/1000000, mean_imp_firm_to_firm = mean_imp_firm_to_firm/1000000) %>% 
+    rename(`No. Indonesian Firms` = n_domestic_firms, `No. US Firms` = n_foreign_firms, `Avg. Import Value per IDN \n Firm to US Industry (USD Million)` = avg_tot_imp_to_f_ind, 
+           `Avg. No US Exporters \n per IDN firm` = avg_n_exporters, `Avg. Value of Firm-to-Firm Imports \n (USD Million)` = mean_imp_firm_to_firm
+    ) %>% 
+    mutate(
+      industry_local = case_when(
+        industry_local == "MANUF" ~ "Manufacturing",
+        industry_local == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_local
+      ),
+      industry_foreign = case_when(
+        industry_foreign == "MANUF" ~ "Manufacturing",
+        industry_foreign == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_foreign
+      )
+    ) %>% 
+    pivot_longer(cols = -c(industry_local, industry_foreign), 
+                 names_to = "variable") %>% 
+    mutate(industries = paste("IDN", industry_local, "-", "US", industry_foreign)) %>% 
+    ggplot(aes(x = variable, y = value, fill = industries))+
+    geom_col(position = position_dodge(width = 1.1), width = 1)+
+    labs(fill = "Bilateral Industry Relation:")+
+    xlab("")+
+    ylab("")+
+    ggtitle("")+
+    theme_minimal()+
+    theme(legend.key.size = unit(0.3, 'cm'), 
+          legend.text = element_text(size = 5), 
+          legend.title = element_text(size = 9, face = "bold"), 
+          strip.text = element_blank(), 
+          axis.text.x = element_text(size = 6), 
+          axis.text.y = element_text(size = 6), 
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(), 
+          plot.title = element_text(size = 12, face = "bold", hjust = 0.5)
+          
+    )+
+    guides(colour = guide_legend(override.aes = list(size = 3)),
+           shape = guide_legend(override.aes = list(size = 3), keyheight = 0.6))+
+    scale_fill_economist()+
+    facet_wrap(~variable, scales = "free", nrow = 3)
+  
+  
+  plot<-ggarrange(
+    p1, p2, labels = c("Exports", "Imports"),
+    common.legend = TRUE, 
+    legend = "right", 
+    font.label = list(size = 12, color = "black", face = "italic", family = NULL)
+    
+  )
+  
+  
+  return(plot)
+}
+
+
+plot_differences<-function(exports_data, imports_data, ylab_title){
+  
+  p1<- exports_data %>% 
+    relocate(matches("yes"), .before = 3) %>% 
+    mutate(
+      industry_local = case_when(
+        industry_local == "MANUF" ~ "Manufacturing",
+        industry_local == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_local
+      ),
+      industry_foreign = case_when(
+        industry_foreign == "MANUF" ~ "Manufacturing",
+        industry_foreign == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_foreign
+      ), 
+      across(c(contains("avg_tot"), contains("mean")), ~./1000000),
+      # Take differences 
+      diff_n_domestic_firms = n_domestic_firms_yes - n_domestic_firms_non, 
+      diff_avg_tot_exp_to_f_ind = avg_tot_exp_to_f_ind_yes - avg_tot_exp_to_f_ind_non, 
+      diff_avg_n_importers = avg_n_importers_yes - avg_n_importers_non, 
+      diff_mean_exp_firm_to_firm = mean_exp_firm_to_firm_yes - mean_exp_firm_to_firm_non
+    ) %>%
+    select(industry_local, industry_foreign, starts_with("diff")) %>% 
+    rename(`Difference No. \n Indonesian Firms` = diff_n_domestic_firms, 
+           `Difference Avg. Export Value \n per IDN Firm to US Industry \n (USD Million)` = diff_avg_tot_exp_to_f_ind, 
+           `Difference Avg. No US \n Importers per IDN firm` = diff_avg_n_importers,
+           `Difference Avg. Value of \n Firm-to-Firm Exports \n (USD Million)` = diff_mean_exp_firm_to_firm) %>% 
+    pivot_longer(cols = -c(industry_local, industry_foreign), names_to = "variable") %>% 
+    mutate(industries = paste("IDN", industry_local, "-", "US", industry_foreign)) %>% 
+    ggplot(aes(x = variable, y = value, fill = industries))+
+    geom_col(position = position_dodge(width = 1.1), width = 1)+
+    labs(fill = "Bilateral Industry Relation:")+
+    ylab(ylab_title)+
+    xlab("")+
+    ggtitle("")+
+    theme_minimal()+
+    theme(legend.key.size = unit(0.3, 'cm'), 
+          legend.text = element_text(size = 5), 
+          legend.title = element_text(size = 9, face = "bold"), 
+          strip.text = element_blank(), 
+          axis.text.x = element_text(size = 7), 
+          axis.text.y = element_text(size = 6), 
+          axis.title.y = element_text(size = 7),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank()
+    )+
+    guides(colour = guide_legend(override.aes = list(size = 3)),
+           shape = guide_legend(override.aes = list(size = 3), keyheight = 0.6))+
+    scale_fill_economist()+
+    facet_wrap(~variable, scales = "free", nrow = 2)+
+    scale_y_continuous()
+  
+  
+  
+  p2 <- imports_data %>% 
+    relocate(matches("yes"), .before = 3) %>% 
+    mutate(
+      industry_local = case_when(
+        industry_local == "MANUF" ~ "Manufacturing",
+        industry_local == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_local
+      ),
+      industry_foreign = case_when(
+        industry_foreign == "MANUF" ~ "Manufacturing",
+        industry_foreign == "WHL-RT" ~ "Wholesale/Retail",
+        TRUE ~ industry_foreign
+      ), 
+      across(c(contains("avg_tot"), contains("mean")), ~./1000000),
+      # Take differences 
+      diff_n_domestic_firms = n_domestic_firms_yes - n_domestic_firms_non, 
+      diff_avg_tot_imp_to_f_ind = avg_tot_imp_to_f_ind_yes - avg_tot_imp_to_f_ind_non, 
+      diff_avg_n_exporters = avg_n_exporters_yes - avg_n_exporters_non, 
+      diff_mean_imp_firm_to_firm = mean_imp_firm_to_firm_yes - mean_imp_firm_to_firm_non
+    ) %>%
+    select(industry_local, industry_foreign, starts_with("diff")) %>% 
+    rename(`Difference No. \n Indonesian Firms` = diff_n_domestic_firms, 
+           `Difference Avg. Import Value \n per IDN Firm to US Industry \n (USD Million)` = diff_avg_tot_imp_to_f_ind, 
+           `Difference Avg. No US \n Exporters per IDN firm` = diff_avg_n_exporters,
+           `Difference Avg. Value of \n Firm-to-Firm Imports \n (USD Million)` = diff_mean_imp_firm_to_firm) %>% 
+    pivot_longer(cols = -c(industry_local, industry_foreign), names_to = "variable") %>% 
+    mutate(industries = paste("IDN", industry_local, "-", "US", industry_foreign)) %>% 
+    ggplot(aes(x = variable, y = value, fill = industries))+
+    geom_col(position = position_dodge(width = 1.1), width = 1)+
+    labs(fill = "Bilateral Industry Relation:")+
+    ylab("")+
+    xlab("")+
+    ggtitle("")+
+    theme_minimal()+
+    theme(legend.key.size = unit(0.3, 'cm'), 
+          legend.text = element_text(size = 5), 
+          legend.title = element_text(size = 9, face = "bold"), 
+          strip.text = element_blank(), 
+          axis.text.x = element_text(size = 7), 
+          axis.text.y = element_text(size = 6), 
+          axis.title.y = element_text(size = 7),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank()
+    )+
+    guides(colour = guide_legend(override.aes = list(size = 3)),
+           shape = guide_legend(override.aes = list(size = 3), keyheight = 0.6))+
+    scale_fill_economist()+
+    facet_wrap(~variable, scales = "free", nrow = 2)+
+    scale_y_continuous()
+  
+  
+  plot_diff<-ggarrange(
+    p1, p2, labels = c("Exports", "Imports"),
+    common.legend = TRUE, 
+    legend = "right", 
+    font.label = list(size = 12, color = "black", face = "italic", family = NULL)
+  )
+  
+  return(plot_diff)
+  
+}
+
+
+
+
+
+
+
